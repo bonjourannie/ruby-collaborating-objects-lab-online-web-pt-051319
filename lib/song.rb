f76b7c9b0d87bf979fad7859cdde5ced98bb1f93
Annie Truex <artruex@gmail.com>
2019-06-05 19:01:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
   
   def self.new_by_filename(file)
     song_name = file.split(" - ")[1]
-    artist = file.split(" - ")[0]
+    name = file.split(" - ")[0]
     song = self.new(song_name)
   end
   
